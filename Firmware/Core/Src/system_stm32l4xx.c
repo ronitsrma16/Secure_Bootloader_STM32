a14d99d0198665455e47e731cb48b72f93746897
ronitsrma16 <ronitsrma16@gmail.com>
2026-09-10 01:44:02 +0530

    updated firmware default address to 0x08004000, by changing linker script and TAB_OFFSET for vector table

--- Firmware/Core/Src/system_stm32l4xx.c
+++ Firmware/Core/Src/system_stm32l4xx.c
@@ -139,7 +139,7 @@
 #endif /* VECT_TAB_SRAM */
 
 #if !defined(VECT_TAB_OFFSET)
-#define VECT_TAB_OFFSET         0x00000000U     /*!< Vector Table offset field.
+#define VECT_TAB_OFFSET         0x00004000U     /*!< Vector Table offset field.
                                                      This value must be a multiple of 0x200. */
 #endif /* VECT_TAB_OFFSET */
 
